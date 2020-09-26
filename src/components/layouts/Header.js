168749d41d0bbb0d8de96a6b5027c5f5e8c3f3c1
hyj11123 <43002611+hyj11123@users.noreply.github.com>
2020-09-26 20:01:08 +0900

    push

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,8 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 class Header extends React.Component {
   render() {
-    return <div>Header</div>;
+    return (
+      <div>
+        <div className="title"></div>
+        <Link to="/screen06">개봉 예정작</Link>
+        <Link to="/screen07">광고 문의</Link>
+      </div>
+    );
   }
 }
 
