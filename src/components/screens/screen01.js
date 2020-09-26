9e49f79c5de11f4d661a055140a50b62a7cfdf51
ouhouh1936 <ouhouh1936@gamil.com>
2020-09-26 20:47:23 +0900

    a

--- src/components/screens/screen01.js
+++ src/components/screens/screen01.js
@@ -2,8 +2,11 @@ import React from "react";
 
 class screen01 extends React.Component {
   render() {
-    return <div className="screen01 screen">screen01</div>;
+    return (
+      <div className="screen01 screen">
+        <div className="screen01__col1"></div>
+      </div>
+    );
   }
 }
-
 export default screen01;
