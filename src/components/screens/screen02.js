34f841ee77f3bcc1bb7785bd6dd4f84e41d62f17
YERIM <dnaer456@gmail.com>
2020-09-26 21:02:52 +0900

    ss

--- src/components/screens/screen02.js
+++ src/components/screens/screen02.js
@@ -5,8 +5,8 @@ class screen02 extends React.Component {
     return (
       <div className="screen02 screen">
         <div className="screen02__main">
-          <div className="screen2__mockup"></div>
-          <div className="screen02__txt">text</div>
+          <div className="screen2__mockup">스마트폰</div>
+          <div className="screen02__txt">텍스트의 텍스트</div>
         </div>
       </div>
     );
