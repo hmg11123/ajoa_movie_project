7f13d49399288b48c6ce4f705606759a46270d51
ouhouh1936 <ouhouh1936@gamil.com>
2020-09-27 15:24:34 +0900

    h

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -4,6 +4,9 @@ class Header extends React.Component {
   render() {
     return (
       <div className="Header">
+        <div className="logo">
+          <div className="logo__col1"></div>
+        </div>
         <div className="title">
           <div className="menu__col1">개봉 예정작</div>
           <div className="menu__col2">광고 문의</div>
