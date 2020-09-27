60bea7762d18307e6b6a5a7fdfc90c7c0c0c9bd1
hyj11123 <43002611+hyj11123@users.noreply.github.com>
2020-09-27 16:32:22 +0900

    mockup

--- src/components/screens/screen03.js
+++ src/components/screens/screen03.js
@@ -1,8 +1,22 @@
 import React from "react";
+import mockup from "../image/phone__mockup.png";
 
 class screen03 extends React.Component {
   render() {
-    return <div className="screen03 screen">screen03</div>;
+    return (
+      <div className="screen03 screen">
+        <div className="screen03__main">
+          <div className="screen03__mockup">
+            <img src={mockup}></img>
+          </div>
+
+          <div className="screen03__txt">
+            <div className="screen03__txt1">오늘의 영화</div>
+            <div className="screen03__txt2">과연 어떤 영화 동영상일지??</div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
--- src/components/screens/screen06.js
+++ src/components/screens/screen06.js
@@ -2,6 +2,7 @@ import React from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
+import Fade from "react-reveal/Fade";
 
 class screen06 extends React.Component {
   render() {
@@ -15,47 +16,49 @@ class screen06 extends React.Component {
     };
     return (
       <div className="screen06 screen">
-        <div className="title">코로나 무비의 영화를 만나보세요</div>
-        <ul className="content">
-          <li>뽜밤! 영화가 상영됩니다!</li>
-          <li>여러분에 추천하는 영화들을</li>
-          <li>다양한 분야로 보여드리겠습니다</li>
-        </ul>
+        <Fade bottom cascade>
+          <div className="title">코로나 무비의 영화를 만나보세요</div>
+          <ul className="content">
+            <li>뽜밤! 영화가 상영됩니다!</li>
+            <li>여러분에 추천하는 영화들을</li>
+            <li>다양한 분야로 보여드리겠습니다</li>
+          </ul>
 
-        <div className="slick_slider">
-          <Slider {...settings}>
-            <div className="slick__box">
-              <div className="slick__box-content">
-                <img src="/" />
-                <div className="desc"></div>
+          <div className="slick_slider">
+            <Slider {...settings}>
+              <div className="slick__box">
+                <div className="slick__box-content">
+                  <img src="/" />
+                  <div className="desc"></div>
+                </div>
               </div>
-            </div>
-            <div className="slick__box">
-              <div className="slick__box-content">
-                <img src="/" />
-                <div className="desc"></div>
+              <div className="slick__box">
+                <div className="slick__box-content">
+                  <img src="/" />
+                  <div className="desc"></div>
+                </div>
               </div>
-            </div>
-            <div className="slick__box">
-              <div className="slick__box-content">
-                <img src="/" />
-                <div className="desc"></div>
+              <div className="slick__box">
+                <div className="slick__box-content">
+                  <img src="/" />
+                  <div className="desc"></div>
+                </div>
               </div>
-            </div>
-            <div className="slick__box">
-              <div className="slick__box-content">
-                <img src="/" />
-                <div className="desc"></div>
+              <div className="slick__box">
+                <div className="slick__box-content">
+                  <img src="/" />
+                  <div className="desc"></div>
+                </div>
               </div>
-            </div>
-            <div className="slick__box">
-              <div className="slick__box-content">
-                <img src="/" />
-                <div className="desc"></div>
+              <div className="slick__box">
+                <div className="slick__box-content">
+                  <img src="/" />
+                  <div className="desc"></div>
+                </div>
               </div>
-            </div>
-          </Slider>
-        </div>
+            </Slider>
+          </div>
+        </Fade>
       </div>
     );
   }
