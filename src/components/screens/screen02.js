d310103a868371e9cc33bb758df98fa154e0481c
YERIM <dnaer456@gmail.com>
2020-09-27 16:25:20 +0900

    scr2

--- src/components/screens/screen02.js
+++ src/components/screens/screen02.js
@@ -8,8 +8,8 @@ class screen02 extends React.Component {
           <div className="screen2__mockup"></div>
 
           <div className="screen02__txt">
-            <div className="screen02__txt1">txt1</div>
-            <div className="screen02__txt2">txt2</div>
+            <div className="screen02__txt1">오늘의 영화</div>
+            <div className="screen02__txt2">과연 어떤 영화 동영상일지??</div>
           </div>
         </div>
       </div>
