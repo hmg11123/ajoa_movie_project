750a7408c20954765e8f1619269301003522db8b
YERIM <dnaer456@gmail.com>
2020-09-27 15:36:39 +0900

    screen2

--- src/components/screens/screen02.js
+++ src/components/screens/screen02.js
@@ -6,6 +6,7 @@ class screen02 extends React.Component {
       <div className="screen02 screen">
         <div className="screen02__main">
           <div className="screen2__mockup"></div>
+
           <div className="screen02__txt">
             <div className="screen02__txt1">txt1</div>
             <div className="screen02__txt2">txt2</div>
