17b66cbcc5b5c97cb3f724e8ed67b426243b8664
YERIM <dnaer456@gmail.com>
2020-09-27 22:39:04 +0900

    scr3 mockup

--- src/components/screens/screen04.js
+++ src/components/screens/screen04.js
@@ -1,8 +1,21 @@
 import React from "react";
+import mockup from "../image/phone__mockup.png";
 
 class screen04 extends React.Component {
   render() {
-    return <div className="screen04 screen">screen04</div>;
+    return (
+      <div className="screen04 screen">
+        <div className="screen04__main">
+          <div className="scree04__txt"></div>
+        </div>
+
+        <div className="screen04__main">
+          <div className="screen04__mockup">
+            <img src={mockup}></img>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
