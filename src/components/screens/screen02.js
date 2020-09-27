899143089be6e86010f7c437ad6386029beb86ce
YERIM <dnaer456@gmail.com>
2020-09-27 16:56:11 +0900

    arr

--- src/components/screens/screen02.js
+++ src/components/screens/screen02.js
@@ -1,15 +1,18 @@
 import React from "react";
+import mockup from "../image/phone__mockup.png";
 
 class screen02 extends React.Component {
   render() {
     return (
       <div className="screen02 screen">
         <div className="screen02__main">
-          <div className="screen02__mockup"></div>
+          <div className="screen02__mockup">
+            <img src={mockup}></img>
+          </div>
 
           <div className="screen02__txt">
-            <div className="screen02__txt1">오늘의 영화</div>
-            <div className="screen02__txt2">과연 어떤 영화 동영상일지??</div>
+            <div className="screen02__txt1">코로나무비</div>
+            <div className="screen02__txt2">영화의 모든것을 알려준다 !!</div>
           </div>
         </div>
       </div>
