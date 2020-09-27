9003d27230b5736e25d4afb68c8f20edb8c82288
KYJ <ouhouh2222@gmail.com>
2020-09-27 14:51:56 +0900

    a

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -5,9 +5,24 @@ class Footer extends React.Component {
     return (
       <div className="footer">
         <div className="footer1">footer1</div>
+          <div className="footer1__txt1">text</div>
+          <div className="footer1__txt2">text</div>
+
         <div className="footer2">footer2</div>
+          <Button>벚튼 ㅇㅋ 확인</Button>
+          <div className="footer2__text"></div>
+
+
+          <div className="footer2__txt">text</div>
+            <div className="footer3__txt"></div>
+
         <div className="footer3">footer3</div>
+
         <div className="footer4">footer4</div>
+          <div className="footer4__gmail">
+            
+          </div>
+
         </div>
       
       
