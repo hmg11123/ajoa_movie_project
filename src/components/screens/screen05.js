e11ca78e79afe160c0390044cea52f9dbaa91aa9
YERIM <dnaer456@gmail.com>
2020-09-29 20:29:19 +0900

    screen05

--- src/components/screens/screen05.js
+++ src/components/screens/screen05.js
@@ -1,10 +1,20 @@
 import React from "react";
+import mockup from "../image/phone__mockup.png";
 
 class screen05 extends React.Component {
   render() {
     return (
       <div className="screen05 screen">
-        <div className="screen05__main">main</div>
+        <div className="screen05__main">
+          <div className="screen05__mockup">
+            <img src={mockup}></img>
+          </div>
+
+          <div className="screen05__txt">
+            <div className="screen05__txt1"></div>
+            <div className="screen05__txt2"></div>
+          </div>
+        </div>
       </div>
     );
   }
