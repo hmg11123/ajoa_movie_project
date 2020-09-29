d56606ffba0c310cfe0942c47dddfab7fbd74512
YERIM <dnaer456@gmail.com>
2020-09-29 20:47:32 +0900

    mockup

--- src/components/screens/screen05.js
+++ src/components/screens/screen05.js
@@ -11,8 +11,8 @@ class screen05 extends React.Component {
           </div>
 
           <div className="screen05__txt">
-            <div className="screen05__txt1"></div>
-            <div className="screen05__txt2"></div>
+            <div className="screen05__txt1">오은하 멍청이</div>
+            <div className="screen05__txt2">오은하 바보</div>
           </div>
         </div>
       </div>
