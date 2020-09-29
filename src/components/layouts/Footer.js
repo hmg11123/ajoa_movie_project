f145589708f18c78ae83703be5998335e520a031
hyj11123 <43002611+hyj11123@users.noreply.github.com>
2020-09-29 21:05:39 +0900

    footer

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -4,29 +4,29 @@ class Footer extends React.Component {
   render() {
     return (
       <div className="footer">
-        <div className="footer1">
-          <div className="footer1__title">영화 안내</div>
-          <div className="footer1__txt1"></div>
-          <div className="footer1__txt2"></div>
-          <div className="footer1__txt3"></div>
-        </div>
+        <div className="footer__main">
+          <div className="footer1">
+            <div className="footer1__title">영화 안내</div>
+            <ul className="footer1__content">
+              <li className="footer__txt">1</li>
+              <li className="footer__txt">2</li>
+              <li className="footer__txt">3</li>
+            </ul>
+          </div>
 
-        <div className="footer2">
-          <div className="footer2__title">영화 광고 문의</div>
-          <div className="footer2__txt"></div>
-        
-        </div>
+          <div className="footer2">
+            <div className="footer2__title">영화 광고 문의</div>
+            <div className="footer__txt"></div>
+          </div>
 
-        <div className="footer3">
-          <div className="footer3__title">제작자 이메일</div>
-          <div className="footer3__gmail"></div>
+          <div className="footer3">
+            <div className="footer3__title">제작자 이메일</div>
+            <div className="footer3__gmail"></div>
+          </div>
         </div>
-
       </div>
-      
-      
-
-    )}
+    );
+  }
 }
 
 export default Footer;
