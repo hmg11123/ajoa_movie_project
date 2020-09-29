b60765f6cf9255e83b9d0e11ef41054d49e8e4b8
ouhouh1936 <ouhouh1936@gamil.com>
2020-09-29 16:34:57 +0900

    w

--- src/components/screens/screen01.js
+++ src/components/screens/screen01.js
@@ -5,8 +5,13 @@ class screen01 extends React.Component {
     return (
       <div className="screen01 screen">
         <div className="screen01__col1">
-          <div className="screen01__title__col1"></div>
           <div className="screen01__mockup"></div>
+        </div>
+        <div className="main">
+          <div className="main__col1">집콕 생활 하는 시대 </div>
+          <div className="main__col2">영화가 항상 필요한 시대</div>
+          <div className="main__col3">영화가 필요할 땐</div>
+          <div className="main__col4"> 코로나 무비!! </div>
         </div>
       </div>
     );
