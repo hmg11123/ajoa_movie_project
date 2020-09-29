f0ed0e4cf8c50413aaafa1e55ca3c64cc2d847cb
hyj11123 <43002611+hyj11123@users.noreply.github.com>
2020-09-29 20:33:05 +0900

    Header

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -5,13 +5,13 @@ class Header extends React.Component {
   render() {
     return (
       <div className="Header">
-        <div className="logo">
+        <span className="logo">
           <img className="logo__col1" src={logo} />
-        </div>
-        <div className="menu">
-          <div className="menu__col1">개봉 예정작</div>
-          <div className="menu__col2">광고 문의</div>
-        </div>
+        </span>
+        <span className="menu">
+          <span className="menu__col1">개봉 예정작</span>
+          <span className="menu__col2">광고 문의</span>
+        </span>
       </div>
     );
   }
--- src/components/screens/screen01.js
+++ src/components/screens/screen01.js
@@ -2,19 +2,7 @@ import React from "react";
 
 class screen01 extends React.Component {
   render() {
-    return (
-      <div className="screen01 screen">
-        <div className="screen01__col1">
-          <div className="screen01__mockup"></div>
-        </div>
-        <div className="main">
-          <div className="main__col1">집콕 생활 하는 시대 </div>
-          <div className="main__col2">영화가 항상 필요한 시대</div>
-          <div className="main__col3">영화가 필요할 땐</div>
-          <div className="main__col4"> 코로나 무비!! </div>
-        </div>
-      </div>
-    );
+    return <div className="screen01 screen"></div>;
   }
 }
 export default screen01;
