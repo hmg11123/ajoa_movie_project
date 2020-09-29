b4e86892a1b9ac4a073741119697ce6329bb74dc
YERIM <dnaer456@gmail.com>
2020-09-29 21:02:45 +0900

    screen01 mockup

--- src/components/screens/screen01.js
+++ src/components/screens/screen01.js
@@ -1,8 +1,19 @@
 import React from "react";
+import mockup from "../image/phone__mockup.png";
 
 class screen01 extends React.Component {
   render() {
-    return <div className="screen01 screen"></div>;
+    return (
+      <div className="screen01 screen">
+        <div className="screen01__main">
+          <div className="screen01__mockup">
+            <img src={mockup}></img>
+          </div>
+
+          <div className=""></div>
+        </div>
+      </div>
+    );
   }
 }
 export default screen01;
