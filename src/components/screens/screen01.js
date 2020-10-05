cd943dbd3d467b2759ce623a02881eb02477a4a2
YERIM <dnaer456@gmail.com>
2020-10-05 21:42:02 +0900

    fade

--- src/components/screens/screen01.js
+++ src/components/screens/screen01.js
@@ -1,19 +1,23 @@
 import React from "react";
 import mockup from "../image/phone__mockup.png";
+import Fade from "react-reveal/Fade";
 
 class screen01 extends React.Component {
   render() {
     return (
       <div className="screen01 screen">
         <div className="screen01__main">
-          <div className="screen01__txt">
-            <div className="screen01__txt1">Hello</div>
-            <div className="screen01__txt2">phone</div>
-          </div>
-
-          <div className="screen01__mockup">
-            <img src={mockup}></img>
-          </div>
+          <Fade left>
+            <div className="screen01__txt">
+              <div className="screen01__txt1">길게길게길게길게길게</div>
+              <div className="screen01__txt2">목업목업목업목업목업</div>
+            </div>
+          </Fade>
+          <Fade right>
+            <div className="screen01__mockup">
+              <img src={mockup}></img>
+            </div>
+          </Fade>
         </div>
       </div>
     );
