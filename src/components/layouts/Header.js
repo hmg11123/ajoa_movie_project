960406bed91a5ea804e9af42a50cd438dd107ee4
ouhouh1936 <ouhouh1936@gamil.com>
2020-10-05 20:51:47 +0900

    w

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -9,7 +9,7 @@ class Header extends React.Component {
           <img className="logo__col1" src={logo} />
         </span>
         <span className="menu">
-          <span className="menu__col1">개봉 예정작 ㅎㅎ 테스트</span>
+          <span className="menu__col1">개봉 예정작</span>
           <span className="menu__col2">광고 문의</span>
         </span>
       </div>
