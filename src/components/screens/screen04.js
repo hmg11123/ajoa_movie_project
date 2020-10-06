b8961329fe35d1d74544acef34f7ade1dfda9f8b
YERIM <dnaer456@gmail.com>
2020-10-06 22:41:42 +0900

    screen4

--- src/components/screens/screen04.js
+++ src/components/screens/screen04.js
@@ -10,7 +10,12 @@ class screen04 extends React.Component {
           <Fade left>
             <div className="screen04__txt">
               <div className="screen04__txt1">즐거움 2배 기쁨도 2배!</div>
-              <div className="screen04__txt2">무비 선물 받아가세요</div>
+              <div className="screen04__txt2">
+                바쁜 날 빨리 볼 수 있게 영화예고편 다~ 모아봤다!!
+              </div>
+              <div className="screen04__txt3">
+                내가 보고싶은 영화 미리보기 !
+              </div>
             </div>
           </Fade>
           <Fade right>
