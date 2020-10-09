054eda3b30684ebc1e5837443e8c104a4ec8a83f
YERIM <dnaer456@gmail.com>
2020-10-09 14:30:27 +0900

    screen5

--- src/components/screens/screen05.js
+++ src/components/screens/screen05.js
@@ -1,19 +1,24 @@
 import React from "react";
 import mockup from "../image/phone__mockup.png";
+import Fade from "react-reveal/Fade";
 
 class screen05 extends React.Component {
   render() {
     return (
       <div className="screen05 screen">
         <div className="screen05__main">
-          <div className="screen05__mockup">
-            <img src={mockup}></img>
-          </div>
+          <Fade left>
+            <div className="screen05__mockup">
+              <img src={mockup}></img>
+            </div>
+          </Fade>
 
-          <div className="screen05__txt">
-            <div className="screen05__txt1">오은하 멍청이</div>
-            <div className="screen05__txt2">오은하 바보</div>
-          </div>
+          <Fade right>
+            <div className="screen05__txt">
+              <div className="screen05__txt1">오늘의 영화를</div>
+              <div className="screen05__txt2">마음껏 누려보세요 !</div>
+            </div>
+          </Fade>
         </div>
       </div>
     );
