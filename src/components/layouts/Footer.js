5b4eca32085c85cb1aff2de46eb28fe8357bddb5
KYJ <ouhouh2222@gmail.com>
2020-10-09 14:36:19 +0900

    a

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -8,21 +8,21 @@ class Footer extends React.Component {
           <div className="footer1">
             <div className="footer1__title">영화 안내</div>
             <ul className="footer1__content">
-              <li className="footer__txt">① 은하바보</li>
-              <li className="footer__txt">② 은하는 ...응</li>
-              <li className="footer__txt">③ 333333333333응</li>
+              <li className="footer__txt">① 없어요</li>
+              <li className="footer__txt">② 없다구요</li>
+              <li className="footer__txt">③ 뭘 바라는거죠 ?</li>
             </ul>
           </div>
 
           <div className="footer2">
             <div className="footer2__title">영화 광고 문의</div>
-            <div className="footer__txt">은하바보은하바보</div>
+            <div className="footer__txt">주지마세요</div>
           </div>
 
           <div className="footer3">
             <div className="footer3__title">제작자 이메일</div>
             <div className="footer3__gmail">
-            <a href="mailto:faner4567@gmail.com" className="a">faner4567@gmail.com</a>
+              <a href="mailto:faner4567@gmail.com" className="a">faner4567@gmail.com</a>
             </div>
             <div className="footer3__gmail">
               <a href="mailto:4leaf.hmg@gmail.com" className="a">4leaf.hmg@gmail.com</a>
