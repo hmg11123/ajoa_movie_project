886614e3627504ee318afe6944345703c428249c
hyj11123 <43002611+hyj11123@users.noreply.github.com>
2020-10-10 21:30:47 +0900

    screen06

--- src/components/screens/screen06.js
+++ src/components/screens/screen06.js
@@ -28,60 +28,59 @@ class screen06 extends React.Component {
             <Slider {...settings}>
               <div className="slick__box">
                 <div className="slick__box-content">
-                  <img src="/" />
-                  <div className="desc"></div>
+                  <img src="https://pds.joins.com/news/component/htmlphoto_mmdata/201704/06/adce9a2b-777a-416b-8835-1d898c7012ca.jpg" />
+                  <div className="desc">아바타 2</div>
                 </div>
               </div>
 
               <div className="slick__box">
                 <div className="slick__box-content">
-                  <img src="/" />
-                  <div className="desc"></div>
+                  <img src="https://lh3.googleusercontent.com/proxy/FOBQmG7gxu08i2J3xmGy16CTDs0j-Kk_WHKk5ceVpJmYXqCvEMsSMued8IBvUjdpHNC9dMO6kaqpHTFeMnH8aGj_M5Rqz3P4ZYSmKDtSitYE5ZU0FrjHnxu5p55yqR_HlQaa6OUIW1pMWAtXD7Fr6cRAPxNIuTMOxT1fThsmOqCNGOflmstTa8L4K-ekY8wlQfQjLHQaRp5qRxwYgM16Li2UE3PRkW4dB4MhbLxcApImEscJeugEXBD4FhesfgrZhmlX5Rrz7ATyXGwvT-iVhNtVVP64dV3hEP79" />
+                  <div className="desc">콰이어트 플레이스</div>
                 </div>
               </div>
 
               <div className="slick__box">
                 <div className="slick__box-content">
-                  <img src="/" />
-                  <div className="desc"></div>
+                  <img src="http://img.mediapen.com/news/201910/news_465777_1569888582_m.jpg" />
+                  <div className="desc">킹스맨 3</div>
                 </div>
               </div>
 
               <div className="slick__box">
                 <div className="slick__box-content">
-                  <img src="/" />
-                  <div className="desc"></div>
+                  <img src="https://img.hankyung.com/photo/202004/BF.22248407.1-1200x.jpg" />
+                  <div className="desc">탑건 2</div>
                 </div>
               </div>
 
               <div className="slick__box">
                 <div className="slick__box-content">
-                  <img src="/" />
-                  <div className="desc"></div>
+                  <img src="https://blog.kakaocdn.net/dn/kA4d1/btqCLdPFp0z/cBp3Ra5dpBkM2yUK7gjHp0/img.jpg" />
+                  <div className="desc">베놈 2</div>
                 </div>
               </div>
 
               <div className="slick__box">
                 <div className="slick__box-content">
-                  <img src="/" />
-                  <div className="desc"></div>
+                  <img src="https://post-phinf.pstatic.net/MjAyMDA4MTRfMjc5/MDAxNTk3MzcxMzE2OTYz.uxrelGcOlSsol1Iws7uISP829J9xs2sk0RD6825fz3cg.ZAaJrEo5oiKG2FL_ksWP1aaO-rlvb8aUEzJmBm05gBQg.JPEG/6.jpg?type=w800_q75" />
+                  <div className="desc">미션임파서블 7</div>
                 </div>
               </div>
 
               <div className="slick__box">
                 <div className="slick__box-content">
-                  <img src="/" />
-                  <div className="desc"></div>
-                </div>
-              </div>
-              
-              <div className="slick__box">
-                <div className="slick__box-content">
-                  <img src="/" />
-                  <div className="desc"></div>
+                  <img src="https://i.pinimg.com/originals/29/b3/ca/29b3ca6f92bcacabb8649b9fcc37ee5f.jpg" />
+                  <div className="desc">모비우스</div>
                 </div>
               </div>
 
+              <div className="slick__box">
+                <div className="slick__box-content">
+                  <img src="https://post-phinf.pstatic.net/MjAxOTEwMjhfMjM0/MDAxNTcyMjU0MzYxNDQ3.VzAaUt_Ej9wsJLY2ay22Zi6xsGeTxh8Xgzd9En7PloMg.0ztm0_U0EGQnrNBezLNkmPwBFWc1P3GsHolb254Zokog.JPEG/0.jpg?type=w1200" />
+                  <div className="desc">노타임 투 다이</div>
+                </div>
+              </div>
             </Slider>
           </div>
         </Fade>
