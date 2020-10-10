e22da794a29fea69b23b4bcf460a4ca4caff944a
ouhouh1936 <ouhouh1936@gamil.com>
2020-10-10 15:34:12 +0900

    asdfgb

--- src/components/screens/screen02.js
+++ src/components/screens/screen02.js
@@ -15,13 +15,11 @@ class screen02 extends React.Component {
           </Fade>
           <Fade right>
             <div className="screen02__txt">
-              <div className="screen02__txt1">1982년 충청도를</div>
-              <div className="screen02__txt1">뒤흔든 전설의</div>
-              <div className="screen02__txt1">대박사건!!</div>
+              <div className="screen02__txt1">요즘 유행인 코로나</div>
+              <div className="screen02__txt1">유행하는 코로나 무비</div>
+              <div className="screen02__txt1"></div>
 
-              <div className="screen02__txt2">
-                요즘 연애는 연애도 아니여~~!!
-              </div>
+              <div className="screen02__txt2"></div>
             </div>
           </Fade>
         </div>
