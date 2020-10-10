2ddc3c90568637551da1bf70222a9fe039c09581
ouhouh1936 <ouhouh1936@gamil.com>
2020-10-10 16:39:26 +0900

    src/qwergthyjuiklo

--- src/components/screens/screen02.js
+++ src/components/screens/screen02.js
@@ -15,9 +15,9 @@ class screen02 extends React.Component {
           </Fade>
           <Fade right>
             <div className="screen02__txt">
-              <div className="screen02__txt1">요즘 유행인 코로나</div>
-              <div className="screen02__txt1">유행하는 코로나 무비</div>
-              <div className="screen02__txt1"></div>
+              <div className="screen02__txt1">전세계적으로 유행하는 코로나</div>
+              <div className="screen02__txt1">그리고 전세계적으로</div>
+              <div className="screen02__txt1">유행할 코로나 무비</div>
 
               <div className="screen02__txt2"></div>
             </div>
