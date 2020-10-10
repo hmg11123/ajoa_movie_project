063f85ab509257e568f5705ea71e42b3718a3139
ouhouh1936 <ouhouh1936@gamil.com>
2020-10-10 20:01:03 +0900

    qwrtyui

--- src/components/screens/screen02.js
+++ src/components/screens/screen02.js
@@ -19,8 +19,9 @@ class screen02 extends React.Component {
               <div className="screen02__txt1">전세계적으로</div>
               <div className="screen02__txt1">유행하는 코로나 무비</div>
 
-              <div className="screen02__txt2">
-                코로나19 확진자를 완치 응원 하고자 만든 사이트입니다.
+              <div className="screen02__txt2">코로나19 확진자를 완치</div>
+              <div className="screen02__txt3">
+                응원 하고자 만든 사이트입니다.
               </div>
             </div>
           </Fade>
